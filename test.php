1a223b45c83179d4f4a55a7d81036a89fd83ca0f
orsifrancesco <hello@orsi.me>
2021-11-07 01:43:38 +0000

    update

--- test.php
+++ test.php
@@ -6,17 +6,17 @@ use InstagramWithoutApi\Fetch;
 
 echo Fetch::fetch([
 
-	"cookie" =>								// <!-- required!! please check https://github.com/orsifrancesco/instagram-without-api to see how to get your cookie
+	"cookie" =>									// <!-- required!! please check https://github.com/orsifrancesco/instagram-without-api to see how to get your cookie
 	'somethingSuperLongLikeThis=fromYourBrowserConsole; csrftoken=1234xcj5; fbm_124024574287414=base_domain=.instagram.com; shbid="12383\0543952162074\054166446......."',
 
-	"maxImages" => 12,						// <!-- optional, 12 is the max number
-	"base64images" => false,				// <!-- optional, false is by default, includes images base64 in the json output, it is pretty heavy
-	"file" => "instagram-cache.json",		// <!-- optional, instagram-cache.json is by default
-	"time" => 3600,							// <!-- optional, reload contents after 3600 seconds by default
-	"pretty" => false,						// <!-- optional, prettyfy json true/false
+	"maxImages" => 12,							// <!-- optional, 12 is the max number
+	"base64images" => false,					// <!-- optional, false is by default, includes images base64 in the json output, it is pretty heavy
+	"file" => "instagram-cache.json",			// <!-- optional, instagram-cache.json is by default
+	"time" => 3600,								// <!-- optional, reload contents after 3600 seconds by default
+	"pretty" => false,							// <!-- optional, prettyfy json true/false
 
-	"id" => "orsifrancesco",				// <!-- id or tag is required
-	//"tag" => "landscape"					// <!-- id or tag is required
+	"id" => "orsifrancesco",					// <!-- id or tag is required
+	//"tag" => "landscape"						// <!-- id or tag is required
 
 ]);
 
